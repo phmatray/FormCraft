e704d8a1922ad64e722e0b4106677be459fc0ea4
Philippe Matray <phmatray@gmail.com>
2025-06-06 13:41:01 +0200

    feat: enable prism for documentation pages

--- DynamicFormBlazor/Services/MarkdownService.cs
+++ DynamicFormBlazor/Services/MarkdownService.cs
@@ -1,4 +1,7 @@
 using Markdig;
+using Markdig.Renderers;
+using Markdig.Renderers.Html;
+using Markdig.Syntax;
 
 namespace DynamicFormBlazor.Services;
 
@@ -23,7 +26,30 @@ public class MarkdownService : IMarkdownService
 
     public string ToHtml(string markdown)
     {
-        return Markdown.ToHtml(markdown, _pipeline);
+        var html = Markdown.ToHtml(markdown, _pipeline);
+        
+        // Post-process to add Prism.js classes
+        html = AddPrismClasses(html);
+        
+        return html;
+    }
+    
+    private string AddPrismClasses(string html)
+    {
+        // Replace code blocks with Prism.js classes
+        html = html.Replace("<code class=\"language-csharp\"", "<code class=\"language-csharp line-numbers\"");
+        html = html.Replace("<code class=\"language-cs\"", "<code class=\"language-csharp line-numbers\"");
+        html = html.Replace("<code class=\"language-razor\"", "<code class=\"language-razor line-numbers\"");
+        html = html.Replace("<code class=\"language-html\"", "<code class=\"language-markup line-numbers\"");
+        html = html.Replace("<code class=\"language-xml\"", "<code class=\"language-markup line-numbers\"");
+        html = html.Replace("<code class=\"language-json\"", "<code class=\"language-json line-numbers\"");
+        html = html.Replace("<code class=\"language-javascript\"", "<code class=\"language-javascript line-numbers\"");
+        html = html.Replace("<code class=\"language-js\"", "<code class=\"language-javascript line-numbers\"");
+        
+        // Add Prism class to pre elements
+        html = html.Replace("<pre><code class=\"language-", "<pre class=\"line-numbers\"><code class=\"language-");
+        
+        return html;
     }
 
     public async Task<string> LoadDocumentAsync(string fileName)
