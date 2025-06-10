1fc0fae853ea859340c006c63e5990216efd6f2e
Philippe Matray <phmatray@gmail.com>
2025-06-10 10:19:35 +0200

    fix: custom renderers does not render correctly

--- FormCraft.ForMudBlazor/Features/CustomFields/MudBlazorRatingRenderer.cs
+++ FormCraft.ForMudBlazor/Features/CustomFields/MudBlazorRatingRenderer.cs
@@ -1,9 +1,29 @@
+using Microsoft.AspNetCore.Components;
+
 namespace FormCraft.ForMudBlazor;
 
 /// <summary>
 /// MudBlazor implementation of the rating field renderer.
+/// Can be used as a custom renderer for int fields to provide star rating.
 /// </summary>
-public class MudBlazorRatingRenderer : FieldRendererBase
+public class MudBlazorRatingRenderer : CustomFieldRendererBase<int>
+{
+    /// <inheritdoc />
+    public override RenderFragment Render(IFieldRenderContext context)
+    {
+        return builder =>
+        {
+            builder.OpenComponent(0, typeof(MudBlazorRatingComponent<>).MakeGenericType(context.Model.GetType()));
+            builder.AddAttribute(1, "Context", context);
+            builder.CloseComponent();
+        };
+    }
+}
+
+/// <summary>
+/// Alternative rating renderer that extends FieldRendererBase for automatic registration.
+/// </summary>
+public class MudBlazorRatingFieldRenderer : FieldRendererBase
 {
     /// <inheritdoc />
     protected override Type ComponentType => typeof(MudBlazorRatingComponent<>);
