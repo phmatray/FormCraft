a216d7af061aadbf17b61fa4e19c9a3ea82e1f98
Philippe Matray <phmatray@gmail.com>
2026-08-11 11:51:27 +0200

    feat(mudblazor): add form-level DefaultShrinkLabel parameter

--- FormCraft.ForMudBlazor.UnitTests/Fields/ShrinkLabelConfigurationTests.cs
+++ FormCraft.ForMudBlazor.UnitTests/Fields/ShrinkLabelConfigurationTests.cs
@@ -70,6 +70,89 @@ public class ShrinkLabelConfigurationTests : MudBlazorTestBase
         component.FindComponent<MudTextField<string>>().Instance.ShrinkLabel.ShouldBeTrue();
     }
 
+    [Fact]
+    public void DefaultShrinkLabel_False_Should_Apply_To_Fields_Without_Explicit_Setting()
+    {
+        // Arrange
+        var model = new TestModel();
+        var config = FormBuilder<TestModel>
+            .Create()
+            .AddField(x => x.Name, field => field.WithLabel("Name"))
+            .Build();
+
+        // Act
+        var component = Render<FormCraftComponent<TestModel>>(parameters => parameters
+            .Add(p => p.Model, model)
+            .Add(p => p.Configuration, config)
+            .Add(p => p.DefaultShrinkLabel, false));
+
+        // Assert
+        component.FindComponent<MudTextField<string>>().Instance.ShrinkLabel.ShouldBeFalse();
+    }
+
+    [Fact]
+    public void FieldLevel_False_Should_Override_FormLevel_True()
+    {
+        // Arrange
+        var model = new TestModel();
+        var config = FormBuilder<TestModel>
+            .Create()
+            .AddField(x => x.Name, field => field
+                .WithLabel("Name")
+                .WithShrinkLabel(false))
+            .Build();
+
+        // Act - form says true (the default), field says false
+        var component = Render<FormCraftComponent<TestModel>>(parameters => parameters
+            .Add(p => p.Model, model)
+            .Add(p => p.Configuration, config)
+            .Add(p => p.DefaultShrinkLabel, true));
+
+        // Assert
+        component.FindComponent<MudTextField<string>>().Instance.ShrinkLabel.ShouldBeFalse();
+    }
+
+    [Fact]
+    public void FieldLevel_True_Should_Override_FormLevel_False()
+    {
+        // Arrange - the other direction, which a null-coalescing bug would silently break
+        var model = new TestModel();
+        var config = FormBuilder<TestModel>
+            .Create()
+            .AddField(x => x.Name, field => field
+                .WithLabel("Name")
+                .WithShrinkLabel(true))
+            .Build();
+
+        // Act
+        var component = Render<FormCraftComponent<TestModel>>(parameters => parameters
+            .Add(p => p.Model, model)
+            .Add(p => p.Configuration, config)
+            .Add(p => p.DefaultShrinkLabel, false));
+
+        // Assert
+        component.FindComponent<MudTextField<string>>().Instance.ShrinkLabel.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void DefaultShrinkLabel_Should_Default_To_True()
+    {
+        // Arrange - a form that never mentions ShrinkLabel keeps the v3.1.0 rendering
+        var model = new TestModel();
+        var config = FormBuilder<TestModel>
+            .Create()
+            .AddField(x => x.Name, field => field.WithLabel("Name"))
+            .Build();
+
+        // Act
+        var component = Render<FormCraftComponent<TestModel>>(parameters => parameters
+            .Add(p => p.Model, model)
+            .Add(p => p.Configuration, config));
+
+        // Assert
+        component.Instance.DefaultShrinkLabel.ShouldBeTrue();
+    }
+
     private class TestModel
     {
         public string Name { get; set; } = string.Empty;
